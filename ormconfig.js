391d2857a89f63dc3f194fbd2c2e317dca1e8fd0
Alyson Maruyama <alyson1907@gmail.com>
2020-10-16 18:00:01 -0300

    Adds rejectUnauthorized to ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,7 @@
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: true },
   entities: ['src/models/**/*.ts'],
   migrations: ['src/migration/**/*.ts'],
   subscribers: ['src/subscriber/**/*.ts'],
