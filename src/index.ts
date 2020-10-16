93581c34fad4cc92950c1dc55387ae31aa981849
Alyson Maruyama <alyson1907@gmail.com>
2020-10-16 17:11:45 -0300

    Updates database initialization with ormconfig file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import './initDB'
+import 'reflect-metadata'
 import express from 'express'
 import router from './routes'
 import cors from 'cors'
--- src/initDB.ts
+++ src/initDB.ts
@@ -1,12 +1,2 @@
 import { createConnection } from 'typeorm'
-
-const config = {
-  url: process.env.DATABASE_URL,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_DBNAME,
-}
-
-console.log(config)
-
-// createConnection(config)
+createConnection()
