804e4ca2493c806a55d52133119a428fd3045719
Alyson Maruyama <alyson1907@gmail.com>
2020-10-16 18:06:39 -0300

    db config test

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: true },
+  ssl: { rejectUnauthorized: false },
   entities: ['src/models/**/*.ts'],
   migrations: ['src/migration/**/*.ts'],
   subscribers: ['src/subscriber/**/*.ts'],
