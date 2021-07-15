75ddba6fd2ee5ac9aae9fc8223344e4af77c9fba
Alyson Matheus <alyson1907@gmail.com>
2021-07-15 19:18:01 -0300

    fixes orm?

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,11 @@
-import { join } from 'path'
+const { join } = require('path')
 
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
   // ssl: { rejectUnauthorized: false },
-  entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+  // entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+  entities: ['build/**/*.entity.{ts,js}'],
   migrations: ['src/migration/**/*.{ts,js}'],
   subscribers: ['src/subscriber/**/*.{ts,js}'],
 }
